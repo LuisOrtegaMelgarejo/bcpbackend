0ba4c94b242559858e04776baa7ea7fff0ef9fa1
LuisOrtegaMelgarejo <luis_ortega_m@hotmail.com>
2021-12-15 16:09:15 -0500

    fix logs to not authorized users

--- src/main/java/com/bcp/exchangeRate/application/ports/in/LoggingUseCase.java
+++ src/main/java/com/bcp/exchangeRate/application/ports/in/LoggingUseCase.java
@@ -1,9 +1,10 @@
 package com.bcp.exchangeRate.application.ports.in;
 
+import com.bcp.exchangeRate.application.domains.entities.Log;
 import io.reactivex.Single;
 
 import javax.servlet.http.HttpServletRequest;
 
 public interface LoggingUseCase {
-    Single<Void> addLogFromRequest(String user, HttpServletRequest req);
+    Single<Log> addLogFromRequest(String user, HttpServletRequest req);
 }
